90f157079c4643127d525b350d26534a5abf23fd
RistoFlink <kenssi@gmail.com>
2023-09-02 14:49:12 +0300

    Add functionality to update student grades

--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GradeController {
@@ -15,6 +16,12 @@ public class GradeController {
 
     @PostMapping("/handleSubmit")
     public String submitForm(Grade grade) {
+        int index = getGradeIndex(grade.getName());
+        if (index == -1000) {
+            studentGrades.add(grade);
+        } else {
+            studentGrades.set(index, grade);
+        }
         studentGrades.add(grade);
         return "redirect:/grades"; // redirects the users by a GET request on /grades to trigger a different
                                    // handler method
@@ -32,9 +39,18 @@ public class GradeController {
     }
 
     @GetMapping("/")
-    public String gradeForm(Model model) {
-        model.addAttribute("grade", new Grade());
+    public String getForm(Model model, @RequestParam(required = false) String name) {
+        model.addAttribute("grade",
+                getGradeIndex(name) == -1000 ? new Grade() : studentGrades.get(getGradeIndex(name)));
         return "form";
+    }
+
+    public Integer getGradeIndex(String name) {
+        for (int i = 0; i < studentGrades.size(); i++) {
+            if (studentGrades.get(i).getName().equals(name))
+                return i;
+        }
+        return -1000;
     }
 
 }
